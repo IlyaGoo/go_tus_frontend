27b4599a1851cef193cc639e2ed5aa8387c30aae
IlyaGo <ilyagooooo@yandex.ru>
2021-10-27 14:23:49 +0500

    Complete simple example

--- src/App.js
+++ src/App.js
@@ -1,32 +1,64 @@
 import logo from './logo.png';
 import './App.css';
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { FiThumbsUp, FiThumbsDown } from 'react-icons/fi';
 
 const App = () => {
 
-  const [voted, setVoted] = useState(['']);
+  const [users, setUsers] = useState([]);
 
-  const [users, setUsers] = useState([
-    {
-      id: 1,
-      Name: "Chel1",
-      Slug: "chel1",
-      Description:
-        "I am Chel1",
-    },
-    {
-      id: 2,
-      Name: "Chel2",
-      Slug: "chel1",
-      Description:
-        "I am Chel2",
-    },
-  ]);
+  const [voted, setVoted] = useState({
+    "dolbaeb": "",
+    "svarshik": "",
+    "yurin_roman": "",
+    "billy_harington": "",
+    "master_van": ""
+  });
 
-  const vote = (type) => {
-    alert(type);
-  }
+  useEffect(() => {
+    const getUsers = async () => {
+      try {
+        const response = await fetch("http://localhost:8003/users", {
+          headers: {
+
+          },
+        });
+
+        const responseData = await response.json();
+
+        setUsers(responseData);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    getUsers();
+  }, []);
+
+  const vote = async (slug, type, index) => {
+    try {
+      const response = await fetch(
+        `http://localhost:8003/users/${slug}/abboutme`,
+        {
+          method: "POST",
+          headers: {
+          },
+          body: JSON.stringify({ vote: type }),
+        }
+      );
+      // Since this is just for demonstration and we're not actually
+      // persisting this data, we'll just set the product vote status here
+      // if the product exists
+      if (response.ok) {
+        setVoted({
+          ...voted,
+          [slug]: [type],
+        });
+      } else console.log(response.status);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <div className="App">
@@ -41,20 +73,21 @@ const App = () => {
           Важная ссылка
         </a>
         <div className="row">
-          {users.map(function (user) {
+          {users.map(function (user, index) {
+            const userSlug = user.Slug;
             return (
-              <div className="col-sm-4">
-                <div className="card">
-                  <div className="card-header">
-                    {user.Name}
-                    <span className="float-left">{voted}</span>
-                  </div>
-                  <div className="card-body">{user.Description}</div>
+              <div className="col-sm-4" key={index}>
+                <div className="card mb-4">
+                  <div className="card-header">{user.Nickname}</div>
+                  <div className="card-body">{user.AboutMe}</div>
                   <div className="card-footer">
-                    <a onClick={() => {}} className="btn btn-default float-left">
-                        <FiThumbsUp />
+                  <a onClick={() => vote(user.Slug, "Upvoted", index)}
+                      className="btn btn-default float-left">
+                      <FiThumbsUp />
                     </a>
-                    <a onClick={() => {}} className="btn btn-default float-right">
+                    <small className="text-muted">{voted[userSlug]}</small>
+                    <a onClick={() => vote(user.Slug, "Downvoted", index)}
+                      className="btn btn-default float-right">
                       <FiThumbsDown />
                     </a>
                   </div>
